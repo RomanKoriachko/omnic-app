d13351e04bed220ab0f2513adeb3540d57ec85da
Roman <mazaxaka.tyt@gmail.com>
2023-06-21 19:48:37 +0300

    add adaptive in Home

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -7,25 +7,27 @@ type Props = {}
 const Home = (props: Props) => {
     return (
         <div className="home">
-            <p className="home-title">Вітаємо вас!</p>
-            <p className="home-subtitle">Що відправлятимемо сьогодні?</p>
-            <div className="row home-items-wrapper">
-                <div className="home-item">
-                    <img src="images/post-machine.png" alt="" />
-                    <Link to={'/package'}>
+            <div className="container">
+                <p className="home-title">Вітаємо вас!</p>
+                <p className="home-subtitle">Що відправлятимемо сьогодні?</p>
+                <div className="row home-items-wrapper">
+                    <div className="home-item">
+                        <img src="images/post-machine.png" alt="" />
+                        <Link to={'/package'}>
+                            <div className="home-btn">
+                                <p className="home-btn-text">
+                                    Надіслати посилку через поштомат
+                                </p>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="home-item">
+                        <img src="images/shop.png" alt="" />
                         <div className="home-btn">
                             <p className="home-btn-text">
-                                Надіслати посилку через поштомат
+                                Повернення до інтернет магазину
                             </p>
                         </div>
-                    </Link>
-                </div>
-                <div className="home-item">
-                    <img src="images/shop.png" alt="" />
-                    <div className="home-btn">
-                        <p className="home-btn-text">
-                            Повернення до інтернет магазину
-                        </p>
                     </div>
                 </div>
             </div>
